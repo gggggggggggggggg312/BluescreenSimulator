c1963b4197cdc4a67c2572959c2574cfd5d9461f
gggggggggggggggg312 <153889123+gggggggggggggggg312@users.noreply.github.com>
2025-12-24 11:19:20 +0700

    Create Windows11Bluescreen.cs

---
+++ BluescreenSimulator/Models/Windows11Bluescreen.cs
@@ -0,0 +1,8 @@
+namespace BluescreenSimulator
+{
+    public class Windows11Bluescreen : Windows10Bluescreen
+    {
+        // No extra code needed (yet)
+        // Windows 11 (2025) is a visual + text evolution
+    }
+}
